b7b14c58eebc8c3f8c67e24bb609a09f0dffdb4e
LightmanM <36053940+lightmanM@users.noreply.github.com>
2025-01-19 22:46:57 -0800

    chore(zai): bridge

--- scripts/layer0_bridge_data.ts
+++ scripts/layer0_bridge_data.ts
@@ -4,7 +4,8 @@ const CHAIN_CONFIG: any = {
         lzEndpointIdOnCurrentChain: 30110,
     },
     BASE: {
-        oftContractAddress: "0x333333c465a19c85f85c6cfbed7b16b0b26e3333",
+        tokenContractAddress: "0x102Cd3E9E14810cE6f0765227E971432BCE05d6C",
+        oftContractAddress: "0xe974eC9A526ba233B8D1359bCAa83d4F9c702Cc7",
         lzEndpointIdOnCurrentChain: 30184,
     },
     BINANCE: {
@@ -12,12 +13,11 @@ const CHAIN_CONFIG: any = {
         lzEndpointIdOnCurrentChain: 30102,
     },
     ETHEREUM: {
-        tokenContractAddress: "0x33333333fede34409fb7f67c6585047e1f653333",
         oftContractAddress: "0x5e1f75388dc768f6129b9ee859bfb1361631a77e",
         lzEndpointIdOnCurrentChain: 30101,
     },
     SOLANA: {
-        oftContractAddress: "BVf9xuRTRLmb8DjfaxDgPuGBdy9qTnspLwYpDpfdUoAY",
+        oftContractAddress: "G6qx5Anbzn8tuF1mazD6sYT3crwufhZak3HB1P1nWfhX",
         lzEndpointIdOnCurrentChain: 30168
     }
 };
